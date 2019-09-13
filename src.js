7a88d8209d965cde638f8cde196183a16a21c48e
Vanna <vhluu@ucsd.edu>
2019-09-12 20:05:46 -0700

    using custom element to add emoji-market to calendar

---
+++ src.js
@@ -0,0 +1,23 @@
+const NimblePicker = require('emoji-mart/dist/components/picker/nimble-picker').default
+const React = require('react')
+const { define } = require('remount/es5')
+
+async function main() {
+  const data = await (await fetch('all.json')).json()
+  const Picker = props => (React.createElement(NimblePicker, {
+    set: 'twitter',
+    data,
+    native: true,
+    onSelect: emoji => console.log('emoji selected', emoji),
+    title: 'Emoji',
+    showPreview: true,
+    ...props
+  }))
+
+  define({ 'emoji-picker': Picker})
+
+  const picker = document.createElement('emoji-picker')
+  document.body.appendChild(picker)
+}
+
+main().catch(err => console.error(err))
