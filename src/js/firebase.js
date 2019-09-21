1ad015988e71e3cea98d3da61318e4314231d662
Vanna <vhluu@ucsd.edu>
2019-09-21 16:33:42 -0700

    adding firebase config

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "<API-KEY>",
-  authDomain: "<AUTH-DOMAIN>",
-  databaseURL: "<DB-URL>",
-  projectId: "<PROJECT-ID>",
+  apiKey: "AIzaSyDLNyV5xvQ5zOAlcb04q11yr-0SrLo4XaM",
+  authDomain: "trackthat-9a3ff.firebaseapp.com",
+  databaseURL: "https://trackthat-9a3ff.firebaseio.com",
+  projectId: "trackthat-9a3ff",
   storageBucket: "",
-  messagingSenderId: "<MSG-SENDER-ID>",
-  appId: "<APP-ID>"
+  messagingSenderId: "925564062879",
+  appId: "1:925564062879:web:3699f8c1eebae75d86a6aa"
 };
 
 // Initialize Firebase
