df42112f61dee50be94b48088751be699c965749
Vanna <vhluu@ucsd.edu>
2019-10-10 21:50:33 -0700

    refactoring cal.js file

--- src/js/cal.js
+++ src/js/cal.js
@@ -1,37 +1,40 @@
 import { userId } from './calendar.js';
 import { toggleDayModal } from './day-modal.js';
 
-var tagsList = document.querySelector('.tags-list');
+const tagsList = document.querySelector('.tags-list');
 
-var calendarBoxes = document.querySelectorAll('.calendar .day');
-var calendarBoxNumbers = document.querySelectorAll('.day-number');
+const calendarBoxes = document.querySelectorAll('.calendar .day');
+const calendarBoxNumbers = document.querySelectorAll('.day-number');
 
 var date = new Date();
 var currentMonth = date.getMonth();
 var currentYear = (date.getYear() + 1900);
 
-
+/**
+  * Sets calendar to given month w/ correct dates and tags
+  * TODO: split up & clean up code
+  */
 export function setCalendar(month) {
-  var monthHeader = document.querySelector('.curr-month');
-  var yearHeader = document.querySelector('.curr-year');
+  const monthHeader = document.querySelector('.curr-month');
+  const yearHeader = document.querySelector('.curr-year');
 
-  var currMonth = new Date();
+  let currMonth = new Date();
   currMonth.setMonth(month);
   monthHeader.innerHTML = currMonth.toLocaleString('default', { month: 'long' });
   yearHeader.innerHTML = ` ${currMonth.getYear() + 1900}`;
 
   currMonth.setDate(1); // sets date to the 1st of current month
-  var day = currMonth.getDay(); // gets day of week for the 1st
+  let day = currMonth.getDay(); // gets day of week for the 1st
 
-  var lastDay = new Date(currMonth.getYear(), month + 1, 0); // sets date to last day of current month
-  var daysInMonth = lastDay.getDate(); // gets the number of days in current month
+  let lastDay = new Date(currMonth.getYear(), month + 1, 0); // sets date to last day of current month
+  let daysInMonth = lastDay.getDate(); // gets the number of days in current month
 
-  var prevMonth = new Date();
+  let prevMonth = new Date();
   prevMonth.setMonth(month);
   prevMonth.setDate(0); // sets date to last day of previous month
-  var endOfPrevDate = prevMonth.getDate();
+  let endOfPrevDate = prevMonth.getDate();
 
-  var today = (new Date()).getDate();
+  let today = (new Date()).getDate();
 
   // Sets the calendar days
   for (var i = 0; i < day; i++) { // from end of previous month to 1st of current month
@@ -41,7 +44,7 @@ export function setCalendar(month) {
   }
   for(var i = 0; i < daysInMonth; i++) { // from 1st to end of month
     calendarBoxNumbers[day + i].textContent = i + 1;
-    if((i + 1) == today) calendarBoxes[day + i].classList.add('today');
+    if ((i + 1) == today) calendarBoxes[day + i].classList.add('today');
     calendarBoxes[day + i].setAttribute('data-tag-day', '' + formatDigit(currMonth.getMonth() + 1) + formatDigit(i + 1));
   }
   for (var i = 0; i < (calendarBoxes.length - daysInMonth - day); i++) { // beginning of next month 
@@ -51,11 +54,12 @@ export function setCalendar(month) {
   }
 
 
+  // Gets tags for current month from the database & adds them to the calendar
   dbGetDayTags(userId, formatDigit(date.getMonth() + 1), currentYear).then((taggedDays) => {
-    if(taggedDays) {
-      var tags = Object.keys(taggedDays); // lists of days that have tags
+    if (taggedDays) {
+      const tags = Object.keys(taggedDays); // lists of days that have tags
       tags.forEach((tag) => {
-        var days = Object.keys(taggedDays[tag]);
+        const days = Object.keys(taggedDays[tag]);
         days.forEach((day) => {
           appendDayTag(document.querySelector(`[data-tag-day="${day}"]`), tag);
         });
@@ -63,43 +67,55 @@ export function setCalendar(month) {
     }
   });
 
-  for(var i = 0; i < calendarBoxes.length; i++) { 
-    calendarBoxes[i].addEventListener('click', toggleDayModal); // opens modal when clicking on a calendar day
-  }
+  // Opens modal when clicking on a calendar day
+  calendarBoxes.forEach((calBox) => {
+    calBox.addEventListener('click', toggleDayModal);
+  });
 }
 
-/* Creates tag element and appends it to calendar day the dom */
+
+/**
+  * Creates tag element and appends it to a specific calendar day in the dom.
+  */
 export function appendDayTag(target, tagId) {
-  var toAdd = document.createElement('div');
-  var tagFromList = tagsList.querySelector('#' + tagId);
-  toAdd.className = "day-tag " + tagFromList.getAttribute('data-tag-color');
+  let toAdd = document.createElement('div');
+  let tagFromList = tagsList.querySelector(`#${tagId}`);
+
+  toAdd.className = `day-tag ${tagFromList.getAttribute('data-tag-color')}`;
   toAdd.textContent = tagFromList.getAttribute('data-tag-icon');
-  toAdd.setAttribute("data-day-tag-id", tagId);
+  toAdd.setAttribute('data-day-tag-id', tagId);
   target.querySelector('.day-tags').appendChild(toAdd);
 }
 
 
+/**
+  * Formats the given number as two digits
+  */
 export function formatDigit(num) {
-  return num < 10 ? ('0' + num) : num;
+  return num < 10 ? `0${num}` : num;
 }
 
-var droppedTagId; var droppedTag;
-export function tagDrop(e) { // adds dropped tag to calendar for a specific day
-  if(e.stopPropagation) {
-    e.stopPropagation();
-  }
+
+/*=================== DRAG & DROP ===================*/
+
+/**
+ * Handles drop event on calendar
+ * Adds dropped tag to calendar for that specific day
+ */
+export function tagDrop(e) { 
+  if (e.stopPropagation) { e.stopPropagation(); }
 
   // create tag element
-  if(e.currentTarget.classList.contains("day")) {
+  if (e.currentTarget.classList.contains('day')) {
     e.currentTarget.classList.remove('chosen-day');
-    droppedTagId = e.dataTransfer.getData('text/plain');
-    droppedTag = document.querySelector(`#${droppedTagId}`);
+    let droppedTagId = e.dataTransfer.getData('text/plain');
+    const droppedTag = document.querySelector(`#${droppedTagId}`);
 
-    if(!(e.currentTarget.querySelector(`#day-tag-${droppedTagId}`))) {
-      var toAdd = document.createElement('div');
-      toAdd.className = "day-tag " + droppedTag.getAttribute('data-tag-color');
+    if (!(e.currentTarget.querySelector(`#day-tag-${droppedTagId}`))) {
+      let toAdd = document.createElement('div');
+      toAdd.className = `day-tag ${droppedTag.getAttribute('data-tag-color')}`;
       toAdd.textContent = droppedTag.getAttribute('data-tag-icon');
-      toAdd.setAttribute("data-day-tag-id", droppedTagId);
+      toAdd.setAttribute('data-day-tag-id', droppedTagId);
       e.currentTarget.querySelector('.day-tags').appendChild(toAdd);
 
       // store tag 
@@ -109,26 +125,33 @@ export function tagDrop(e) { // adds dropped tag to calendar for a specific day
 }
 
 
+/**
+ * Handles drag over event on calendar
+ */
 function tagDragOver(e) {
-  if (e.preventDefault) e.preventDefault(); // allows drop
+  if (e.preventDefault) { e.preventDefault(); } // allows drop to happen
 }
 
+
+/**
+ * Handles drag enter event on calendar
+ */
 function tagDragEnter(e) {
-  if(e.target.classList.contains("day")) {
-    e.target.classList.add('chosen-day');
-  }
+  if (e.target.classList.contains('day')) { e.target.classList.add('chosen-day'); }// adds bg color to calendar day
 }
 
+
+/**
+ * Handles drag leave event on calendar
+ */
 function tagDragLeave(e) {
-  if(e.target.classList.contains("day")) {
-    e.target.classList.remove('chosen-day');
-  }
+  if (e.target.classList.contains('day')) {  e.target.classList.remove('chosen-day'); } // removes bg color from calendar day
 }
 
-
-for (var i = 0; i < calendarBoxes.length; i++) {
-  calendarBoxes[i].addEventListener('dragover', tagDragOver, false);
-  calendarBoxes[i].addEventListener('dragenter', tagDragEnter, false);
-  calendarBoxes[i].addEventListener('dragleave', tagDragLeave, false);
-  calendarBoxes[i].addEventListener('drop', tagDrop, false);
-}
+// Adds drag & drop events to each calendar day
+calendarBoxes.forEach((calBox) => {
+  calBox.addEventListener('dragover', tagDragOver, false);
+  calBox.addEventListener('dragenter', tagDragEnter, false);
+  calBox.addEventListener('dragleave', tagDragLeave, false);
+  calBox.addEventListener('drop', tagDrop, false);
+});
