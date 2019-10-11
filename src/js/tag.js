06b77764e4b44dbf488669f0910d23d3dde88d9a
Vanna <vhluu@ucsd.edu>
2019-10-10 23:16:50 -0700

    refactoring tag.js

--- src/js/tag.js
+++ src/js/tag.js
@@ -1,9 +1,15 @@
+/**
+ * Handles drag start event for tag by transferring the tag id
+ */
 export function tagDragStart(e) {
   e.target.classList.add('dragged');
   e.dataTransfer.effectAllowed = 'copyLink';
   e.dataTransfer.setData('text/plain', this.id);
 }
 
+/**
+ * Handles drag end event by removing the dragged state styling
+ */
 export function tagDragEnd(e) {
   e.target.classList.remove('dragged');
 }
