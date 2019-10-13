5ff88869496c4a108538cb9edd6abeaad23f9eb0
Vanna <vhluu@ucsd.edu>
2019-10-12 22:47:47 -0700

    removing preact

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,7 @@ module.exports = {
     ]
   },
   resolve: {
-      alias: {
-          'react': 'preact/compat/dist/compat.js',
-          'react-dom': 'preact/compat/dist/compat.js',
-          'preact/hooks': 'preact/hooks/dist/hooks.js'
-      },
-      extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx']
   },
   plugins: [
     new CopyPlugin([
