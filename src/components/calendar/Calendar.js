4329500aefabce9d48c0be6d696b97ade4fc82d5
Vanna <vhluu@ucsd.edu>
2019-10-13 23:42:09 -0700

    setting calendar days

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Day from '../Day/Day';
 import './Calendar.css';
+import { getData } from 'emoji-mart/dist/utils';
 
 
 class Calendar extends Component {
@@ -9,31 +10,64 @@ class Calendar extends Component {
 
     const date = new Date();
     this.state = {
-      days: [ '10012019', '10022019', '100302019'],
+      days: [],
       date: {
+        full: date,
         month: date.getMonth(),
-        year: date.getFullYear()
+        year: date.getYear() + 1900
       }
     }
+    
   }
 
-  getDays(month, year) {
-                   
+  componentDidMount() {
+    this.setCalendar(this.state.date);
+  }
+
+  /* Sets the calendar to the given month and year */
+  setCalendar({ month, year }) {
+    let days = [];
+    let currMonth = new Date(year, month, 1); // sets date to 1st day of current month
+    let dayOfWeek = currMonth.getDay();
+
+    // if current month doesn't start on Sunday, then get dates from last month 
+    if(dayOfWeek !== 0) { 
+      let endOfLastMonth = (new Date(year, month - 1, 0)).getDate(); // last day of last month
+      for (var i = 0; i < dayOfWeek; i++) {
+        days.push(endOfLastMonth - dayOfWeek + i);
+      }
+    }
+
+    currMonth.setDate(0); // sets date to last day of current month
+    let daysInCurrMonth = currMonth.getDate(); // gets days in current month
+    for (var i = 0; i < daysInCurrMonth; i++) {
+      days.push(i+1);
+    }
+
+    // add days from next month if needed to fill up calendar
+    if (days.length < 35) { 
+      let nextMonth = new Date(year, month + 1, 1);
+      for (var i = 0; i < 35 - days.length; i++) {
+        days.push(i+1);
+      }
+    }
+    
+    this.setState({ days: days })
   }
 
   render() {
+    const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+
     return (
-      <div class="calendar-wrapper">
-        <h1 class="curr-date"><span class="curr-month"></span><span class="curr-year"></span></h1>
-        <div class="calendar">
-          <div class="cal-header">Sun</div>
-          <div class="cal-header">Mon</div>
-          <div class="cal-header">Tue</div>
-          <div class="cal-header">Wed</div>
-          <div class="cal-header">Thur</div>
-          <div class="cal-header">Fri</div>
-          <div class="cal-header">Sat</div>
-          <div class="day"><span class="day-number"></span><div class="day-tags"><div class="day-tag green" data-day-tag-id="aa">🌱</div></div></div>
+      <div className="calendar-wrapper">
+        <h1 className="curr-date">
+          <span className="curr-month">{ this.state.date.full.toLocaleString('default', { month: 'long' }) }</span> <span className="curr-year">{ this.state.date.year }</span>
+        </h1>
+        <div className="calendar">
+          {daysOfWeek.map((day) => 
+            <div className="cal-header">{ day }</div>
+          )}
+          <div className="day"><span className="day-number"></span><div className="day-tags"><div className="day-tag green" data-day-tag-id="aa">🌱</div></div></div>
           {this.state.days.map((day) =>
             <Day date={day} />
           )}
