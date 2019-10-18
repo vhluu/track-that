e3250fae5d6bc74f52dcd4b3293218e644783777
Vanna <vhluu@ucsd.edu>
2019-10-17 19:31:30 -0700

    splitting auth flow into multiple functions

--- src/js/background.js
+++ src/js/background.js
@@ -1,21 +1,7 @@
 var auth_url = 'https://accounts.google.com/o/oauth2/auth?';
-var client_id = '<CLIENT-ID>';
-var redirect_url = chrome.identity.getRedirectURL();
+const client_id = '<CLIENT-ID>';  // must be Web Application type
+const redirect_url = chrome.identity.getRedirectURL(); // make sure to define Authorised redirect URIs in the Google Console such as https://<-your-extension-ID->.chromiumapp.org/
 console.log(redirect_url);
-var auth_params = {
-    client_id: client_id,
-    redirect_uri: redirect_url,
-    response_type: 'code',
-    access_type: 'offline',
-    approval_prompt: 'force',
-    scope: 'email',
-    login_hint: ''
-};
-
-const url = new URLSearchParams(Object.entries(auth_params));
-url.toString();
-auth_url += url;
-
 
 var access_token;
 var refresh_token;
@@ -23,9 +9,31 @@ var refresh_token;
 var email;
 var user_id;
 
-chrome.identity.launchWebAuthFlow({ url: auth_url, interactive: true }, function (responseUrl) {
+
+function _startAuthFlow() {
+  const auth_params = {
+    client_id: client_id,
+    redirect_uri: redirect_url,
+    response_type: 'code',
+    access_type: 'offline',
+    approval_prompt: 'force',
+    scope: 'email',
+    login_hint: '' // fake or non-existent won't work
+  };
+
+  const url = new URLSearchParams(Object.entries(auth_params));
+  url.toString();
+  auth_url += url;
+
+  chrome.identity.launchWebAuthFlow({ url: auth_url, interactive: true }, _getRefreshToken);
+}
+
+function _getRefreshToken(responseUrl) {
+  console.log(responseUrl);
+
   var token_url = 'https://www.googleapis.com/oauth2/v4/token';
-  var auth_code = responseUrl.split("&")[0].split("code=")[1];
+  const auth_code = responseUrl.split("&")[0].split("code=")[1];
+  console.log(auth_code);
 
   var token_params = {
     code: decodeURIComponent(auth_code),
@@ -36,47 +44,60 @@ chrome.identity.launchWebAuthFlow({ url: auth_url, interactive: true }, function
   }
 
   const request_url = new URLSearchParams(Object.entries(token_params));
-  request_url.toString();
 
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open('post', token_url)
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.onload = function() {
     console.log(this.status);
     console.log(this.response);
 
+    // grab the access token and use that to get the email
     var token_info = JSON.parse(this.response);
     refresh_token = token_info.refresh_token;
     access_token = token_info.access_token;
     console.log(refresh_token);
     console.log(access_token);
 
-    var xhr2 = new XMLHttpRequest();
-    var url = 'https://www.googleapis.com/oauth2/v3/userinfo';
-    xhr2.open('get', url);
-    
-    xhr2.setRequestHeader('Authorization', 'Bearer ' + access_token);
-    xhr2.onload = function() {
-      console.log(this.status);
-      // TODO: handling errors
-      if (this.status == 401) {
-        console.log('401 error');
-      } else {
-        if (this.status == 200) {
-          console.log(this.response);
-          var user_info = JSON.parse(this.response);
-          email = user_info.email;
-          user_id = user_info.sub;
-          console.log(email);
-          console.log(user_id);
-          
-        } else {
-          console.log("Error:");
-        }
-      }
-    }
-    xhr2.send();
+    // TODO: store refresh & access token
+
+    // TODO: use refresh token to retrieve access token
+
+    // use access token to receive info
+    _getUserInfo(access_token);
   }
 
   xhr.send(request_url.toString());
-});
+}
+
+function _getUserInfo(access_token) {
+  let xhr = new XMLHttpRequest();
+  var url = 'https://www.googleapis.com/oauth2/v3/userinfo';
+  xhr.open('get', url);
+
+  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+  xhr.onload = function() {
+    console.log(this.status);
+    if (this.status == 401) {
+      //retry = false;
+      //chrome.identity.removeCachedAuthToken({ token: access_token }, getToken);
+      // TODO: revoke auth token and request a new one
+      console.log('401 error');
+    } else {
+      if (this.status == 200) {
+        console.log(this.response);
+        var user_info = JSON.parse(this.response);
+        email = user_info.email;
+        user_id = user_info.sub;
+        console.log(email);
+        console.log(user_id);
+        
+      } else {
+        console.log("Error:");
+      }
+    }
+  }
+  xhr.send();
+}
+
+_startAuthFlow();
