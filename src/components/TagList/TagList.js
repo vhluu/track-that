d23255a93c2c2f89df66da086bf46a5478097d2a
Vanna <vhluu@ucsd.edu>
2019-11-15 23:32:07 -0800

    moving TagModal component and setting up props for TagList

--- src/components/TagList/TagList.js
+++ src/components/TagList/TagList.js
@@ -1,55 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import TagModal from './TagModal/TagModal';
-
 class TagList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      action: 'create',
-      selectedTag: null,
-      tags: [],
-      showModal: false,
-    };
-  }
-
-  componentDidMount() {
-    const { uid } = this.props;
-    if (uid) {
-      this.getTags(uid);
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { uid } = this.props;
-    if (prevProps.uid !== uid) {
-      this.getTags(uid);
-    }
-  }
-
-  /* Gets user data from firebase */
-  getTags(id) {
-    const { firebase } = this.props;
-    if (id) {
-      firebase.dbGetTags(id).then((tags) => {
-        console.log(tags);
-        this.setState({ tags });
-      });
-    }
-  }
-
-  toggleTagModal(tagId) {
-    this.setState((prevState) => ({
-      action: tagId ? 'update' : 'create',
-      selectedTag: tagId,
-      showModal: (prevState.selectedTag === tagId) ? !(prevState.showModal) : true,
-    }));
-  }
-
   render() {
-    const { tags, showModal, action } = this.state;
+    const { tags, onCreateTag, onDeleteTag, onClick } = this.props;
     return (
       <div>
         <div className="tags-list">
@@ -57,17 +11,10 @@ class TagList extends Component {
           {Object.keys(tags).map((id) => (
             <div className={`tag ${tags[id].color}`} draggable="true" id={`t${id}`} data-tag-color={tags[id].color} data-tag-icon={tags[id].icon} data-tag-title={tags[id].title} onClick={this.toggleTagModal.bind(this, id)}>{tags[id].icon} {tags[id].title}</div>
           ))}
-          <div className="tag btn-add-tag" onClick={this.toggleTagModal.bind(this, null)}>+ Add New Tag</div>
         </div>
-        { showModal && <TagModal action={action} /> }
       </div>
     );
   }
 }
-
-TagList.propTypes = {
-  firebase: PropTypes.objectOf(PropTypes.object).isRequired,
-  uid: PropTypes.string.isRequired,
-};
 
 export default TagList;
--- src/containers/TagsContainer.js
+++ src/containers/TagsContainer.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TagList from '../components/TagList/TagList';
+import TagModal from '../components/TagModal/TagModal';
 
 class TagsContainer extends Component {
-	render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      action: 'create',
+      showModal: false,
+    };
+  }
+
+  toggleTagModal(tagId) {
+    this.setState((prevState) => ({
+      action: tagId ? 'update' : 'create',
+      selectedTag: tagId,
+      showModal: (prevState.selectedTag === tagId) ? !(prevState.showModal) : true,
+    }));
+  }
+
+  render() {
+    const { showModal, action } = this.state;
+    const { tags, onCreateTag, onDeleteTag, onUpdateTag } = this.props;
+
     return (
       <div>
         <h2>Tags</h2>
         <p>Drag &amp; drop a tag to add it to the calendar!</p>
-        <TagList />
+        <TagList 
+          tags={tags} 
+          onCreateTag={onCreateTag}
+          onDeleteTag={onDeleteTag}
+          onUpdateTag={onUpdateTag}
+          onClick={this.toggleTagModal} 
+        />
+        <div className="tag btn-add-tag" onClick={this.toggleTagModal.bind(this, null)}>+ Add New Tag</div>
+        { showModal && <TagModal action={action} /> }
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		tags: state.tags,
-	};
-}
+const mapStateToProps = (state) => ({
+  tags: state.tags,
+});
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		onCreateTag: () => dispatch({ type: 'CREATE_TAG' }),
-		onDeleteTag: () => dispatch({ type: 'DELETE_TAG' }),
-		onUpdateTag: () => dispatch({ type: 'UPDATE_TAG' }),
-	}
-};
+const mapDispatchToProps = (dispatch) => ({
+  onCreateTag: () => dispatch({ type: 'CREATE_TAG' }),
+  onDeleteTag: () => dispatch({ type: 'DELETE_TAG' }),
+  onUpdateTag: () => dispatch({ type: 'UPDATE_TAG' }),
+});
 
 
-export default connect(mapStateToProps)(TagsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(TagsContainer);
