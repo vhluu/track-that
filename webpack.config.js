3d254b2fbfb762fadd12c5d2efe8775ab9dc5e42
Vanna <vhluu@ucsd.edu>
2019-11-26 12:14:23 -0800

    removing unused files

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,34 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: {
     index: './src/index.js',
-    calendar: './src/js/calendar.js',
     popup: './src/js/popup.js',
-    background: './src/js/background.js'
+    background: './src/js/background.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'] // use: ['babel-loader', 'eslint-loader']
+        use: ['babel-loader'], // use: ['babel-loader', 'eslint-loader'],  
       },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   plugins: [
     new CopyPlugin([
       { from: 'src', to: '', ignore: ['js/*', 'components/**/*', 'index.js'] },
-      { from: 'src/js/firebase.js', to: 'js' }
     ]),
   ],
   output: {
     filename: 'js/[name].min.js',
     chunkFilename: 'js/[name].min.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-  watch: true
-}
+  watch: true,
+};
