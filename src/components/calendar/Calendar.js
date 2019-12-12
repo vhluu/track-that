2e0d82c72706cfac74358940efc29dbae13b0a22
Vanna <vhluu@ucsd.edu>
2019-12-11 21:02:21 -0800

    adding modal and checkbox components to calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 import Day from './Day/Day';
-import DayModal from './DayModal/DayModal';
+import Modal from '../Modal/Modal';
+import Checkbox from '../Checkbox/Checkbox';
 
 import './Calendar.css';
 
@@ -74,7 +74,20 @@ class Calendar extends Component {
           />
         ))}
 
-        { showDayModal && <DayModal tags={selectedDayTags} /> }
+        { showDayModal && (
+          <Modal show={showDayModal} extraClasses="edit-day-modal">
+            <p className="edit-day"></p>
+            <Checkbox id="check-select-all" extraClasses="select-all">Select All</Checkbox>
+            <div className="day-checkboxes">
+              {selectedDayTags && Object.keys(selectedDayTags).map((tagId) => (
+                <Checkbox id={`check-${tagId}`}>
+                  <div className={`tag ${selectedDayTags[tagId].color}`} draggable="true" id={`${tagId}`} data-tag-color={selectedDayTags[tagId].color} data-tag-icon={selectedDayTags[tagId].icon} data-tag-title={selectedDayTags[tagId].title}>{selectedDayTags[tagId].icon} {selectedDayTags[tagId].title}</div>
+                </Checkbox>
+              ))}
+            </div>
+            <svg className="delete-icon" viewBox="0 0 137.583 164.571" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-42.333 -64.167)"><rect x="52.917" y="112.32" width="116.42" height="116.42" rx="10.583" ry="7.276" fill="#CFD8DC"/><rect  className="delete-icon-top" x="127" y="64.167" width="10.583" height="31.75" rx="4.914" ry="5.291" fill="#CFD8DC"/><g fill="#CFD8DC" className="delete-icon-top"><rect x="84.667" y="64.167" width="10.583" height="31.75" rx="5.291" ry="5.292"/><rect x="42.333" y="85.333" width="137.58" height="20.411" rx="10.583" ry="15.308"/><rect x="84.667" y="64.167" width="52.917" height="10.583" rx="10.583" ry="7.938"/></g><g fill="#B0BEC5"><rect transform="scale(1 -1)" x="105.83" y="-212.86" width="10.583" height="84.667" rx="10.583" ry="7.276"/><rect x="74.083" y="127.67" width="10.583" height="84.667" rx="10.583" ry="7.761"/><rect x="137.58" y="127.67" width="10.583" height="84.667" rx="10.583" ry="7.276"/></g></g></svg>
+          </Modal>
+        ) }
       </div>
     );
   }
