9e5330fe15b02dad7974dc95833c11f32acf253f
Vanna <vhluu@ucsd.edu>
2019-12-28 23:02:53 -0800

    cleaning up popup script

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,101 +1,6 @@
 import db from '../util/firebase';
 
-const calendarBtn = document.querySelector('.view-cal');
-console.log(calendarBtn);
-calendarBtn.addEventListener('click', () => {
-  chrome.tabs.create({ url: chrome.extension.getURL('index.html') }, (tab) => {
-    // Tab opened.
-    console.log(tab);
-  });
-});
-
-const signinBtn = document.querySelector('.google-signin');
-const signoutBtn = document.querySelector('.google-signout');
-
-// we want to display the login
-signinBtn.addEventListener('click', () => {
-  retrieveLoginStatus(true);
-});
-
-signoutBtn.addEventListener('click', () => {
-  signoutUser();
-});
-
-const retrieveLoginStatus = (startLogin) => {
-  chrome.extension.sendMessage({ greeting: 'hello from popup', login: startLogin }, (response) => {
-    if (response && response.email) {
-      console.log(response);
-      if (response.email) {
-        // TODO: change to add hide class
-        signinBtn.style.display = 'none';
-        signoutBtn.style.display = 'block';
-
-        // set the date in the popup template
-        const date = new Date();
-        const fullDate = `${(date.getMonth() + 1) % 13}${date.getYear() + 1900}`;
-        const formattedDay = date.getDate() < 10 ? `0${date}` : date.getDate();
-        const currDate = `${(date.getMonth() + 1) % 13}${formattedDay}`;
-
-        const currDayTags = [];
-        const tagWrapper = document.querySelector('.day-tags');
-
-        // get the tags for the current month
-        // TODO: try getting day tags from storage, if none, then grab from firebase
-        db.ref(`users/${response.userId}/tagged/${fullDate}`).once('value').then((snapshot) => {
-          const dayTags = snapshot.val();
-          if (dayTags) { // get the tag ids for the current day
-            Object.keys(dayTags).forEach((tagId) => {
-              Object.keys(dayTags[tagId]).forEach((day) => {
-                if (day === currDate) {
-                  currDayTags.push(tagId.substring(1));
-                }
-              });
-            });
-
-            // use the tag ids to grab the rest of the tag info from the database
-            if (currDayTags.length > 0) {
-              db.ref(`users/${response.userId}/tags`).once('value').then((snapshot1) => {
-                const tags = snapshot1.val();
-                if (tags) {
-                  const tagData = currDayTags.map((tagId) => tags[tagId]);
-
-                  // create the tags and append to popup template
-                  tagData.forEach((tag) => {
-                    const tagElem = document.createElement('div');
-                    tagElem.textContent = tag.icon;
-                    tagElem.className = `day-tag ${tag.color}`;
-                    tagWrapper.appendChild(tagElem);
-                  });
-
-                  createAddBtn();
-                }
-              });
-            } else {
-              createAddBtn();
-            }
-          } else {
-            createAddBtn();
-          }
-        });
-
-        const dayOfWeek = document.querySelector('.day-of-week');
-        const dayNum = document.querySelector('.day-number');
-        const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
-
-        // sets date in popup template
-        dayOfWeek.textContent = daysOfWeek[date.getDay()];
-        dayNum.textContent = date.getDate();
-      }
-    } else {
-      console.log("Couldn't get email address of profile user.");
-      // TODO: change to add hide class
-      signinBtn.style.display = 'block';
-      signoutBtn.style.display = 'none';
-    }
-  });
-};
-
-// Create add tag button which will open the calendar page on click
+/* Creates "Add Tag" button which will open the calendar page on click */
 const createAddBtn = () => {
   const tagWrapper = document.querySelector('.day-tags');
   const addBtn = document.createElement('div');
@@ -108,21 +13,131 @@ const createAddBtn = () => {
   });
 };
 
-const signoutUser = () => {
-  chrome.extension.sendMessage({ greeting: 'sign me out' }, (response) => {
-    console.log(response);
-    if (response) {
-      console.log(response);
-      if (response.signed_out) {
-        signinBtn.style.display = 'block';
-        signoutBtn.style.display = 'none';
+
+/* The Sign In and Sign Out button elements */
+const signinBtn = document.querySelector('.google-signin');
+const signoutBtn = document.querySelector('.google-signout');
+
+/* Show the Sign In button only */
+const showSignIn = () => {
+  signinBtn.style.display = 'block'; // TODO: change to add hide class
+  signoutBtn.style.display = 'none';
+};
+
+/* Show the Sign Out button only */
+const showSignOut = () => {
+  signinBtn.style.display = 'none';
+  signoutBtn.style.display = 'block';
+};
+
+
+/* Sets the date in the popup template */
+const setDate = () => {
+  const date = new Date();
+  const dayOfWeek = document.querySelector('.day-of-week');
+  const dayNum = document.querySelector('.day-number');
+  const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+
+  // sets date in popup template
+  dayOfWeek.textContent = daysOfWeek[date.getDay()];
+  dayNum.textContent = date.getDate();
+};
+
+
+/* Grabs the tags for the current day and displays it in the popup template */
+const setTags = (userId) => {
+  // generate the dates needed
+  const date = new Date();
+  const month = (date.getMonth() + 1) % 13;
+  const day = date.getDate();
+  const fullDate = `${month}${date.getYear() + 1900}`;
+  const formattedDay = day < 10 ? `0${day}` : day;
+
+  const currDayTags = [];
+
+  // get the tags for the current month from the database
+  // TODO: try getting day tags from storage, if none, then grab from firebase
+  db.ref(`users/${userId}/tagged/${fullDate}`).once('value').then((snapshot) => {
+    const dayTags = snapshot.val();
+    if (dayTags) { // get the tag ids for the current day
+      const currDate = `${month}${formattedDay}`;
+      Object.keys(dayTags).forEach((tagId) => {
+        Object.keys(dayTags[tagId]).forEach((day) => {
+          if (day === currDate) {
+            currDayTags.push(tagId.substring(1));
+          }
+        });
+      });
+
+      // use the tag ids to grab the rest of the tag info from the database
+      if (currDayTags.length > 0) {
+        db.ref(`users/${userId}/tags`).once('value').then((snapshot1) => {
+          const tags = snapshot1.val();
+          if (tags) {
+            const tagData = currDayTags.map((tagId) => tags[tagId]);
+
+            const tagWrapper = document.querySelector('.day-tags');
+            // create the tags and append to popup template
+            tagData.forEach((tag) => {
+              const tagElem = document.createElement('div');
+              tagElem.textContent = tag.icon;
+              tagElem.className = `day-tag ${tag.color}`;
+              tagWrapper.appendChild(tagElem);
+            });
+            createAddBtn();
+          }
+        });
+      } else {
+        createAddBtn();
       }
     } else {
-      console.log("Couldn't sign out. Please try again!");
-      signinBtn.style.display = 'block';
-      signoutBtn.style.display = 'none';
+      createAddBtn();
     }
   });
 };
 
+
+/** Gets the login status of the user
+ * Takes a boolean which indicates whether to start the login progress 
+ */
+const retrieveLoginStatus = (startLogin) => {
+  // communicate to background script that we want to retrieve the login status
+  chrome.extension.sendMessage({ greeting: 'hello from popup', login: startLogin }, (response) => {
+    if (response && response.email) { // if user is signed in
+      console.log(response);
+      showSignOut(); // hide Sign In button and show Sign Out button
+      setTags(response.userId); // sets the tags in the popup template
+    } else {
+      console.log("Couldn't get email address of profile user.");
+      showSignIn();
+    }
+  });
+};
+
+
+/* Signs in the user w/ Sign In button click */
+signinBtn.addEventListener('click', () => {
+  retrieveLoginStatus(true);
+});
+
+/* Signs out user w/ Sign Out button click */
+signoutBtn.addEventListener('click', () => {
+  // communicate to background script that user wants to sign out
+  chrome.extension.sendMessage({ greeting: 'sign me out' }, (response) => {
+    // if the background script succeeds in signing out the user
+    if (response && response.signed_out) {
+      showSignOut();
+    }
+  });
+});
+
+
+/* Open the calendar page in a new tab when clicking on the View Calendar button */
+const calendarBtn = document.querySelector('.view-cal');
+calendarBtn.addEventListener('click', () => {
+  chrome.tabs.create({ url: chrome.extension.getURL('index.html') });
+});
+
+
 retrieveLoginStatus(false);
+setDate();
