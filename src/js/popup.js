86499d621e4ea7c48956fa6bbbe33c24c44d3a63
Vanna <vhluu@ucsd.edu>
2020-01-14 21:36:15 -0800

    fixing incorrect month format for popup

--- src/js/popup.js
+++ src/js/popup.js
@@ -48,7 +48,9 @@ const setDate = () => {
 const setTags = (userId) => {
   // generate the dates needed
   const date = new Date();
-  const month = (date.getMonth() + 1) % 13;
+  let month = (date.getMonth() + 1) % 13;
+  if (month < 10) month = `0${month}`;
+
   const day = date.getDate();
   const fullDate = `${month}${date.getYear() + 1900}`;
   const formattedDay = day < 10 ? `0${day}` : day;
