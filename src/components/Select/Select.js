62aa223ebd6e6ac26f081de369acf2cbd90ec978
Vanna <vhluu@ucsd.edu>
2020-02-19 12:58:13 -0800

    handling select click events

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Select.scss';
 
-function Select(props) {
-  // const { options, value, onChange } = props;
-  const options = [ { label: '😃 hello', value: 't1' }, { label: '🌊 goodbye', value: 't2' } ];
-  return (
-    <div className="select">
-      <div className="option-current">{ (options && options[0]) ? options[0].label : 'No Tags!' }</div>
-      <div className="options-wrapper">
-        { options.map((option) => <div className="option" data-value={option.value}>{ option.label }</div>) }
-      </div>
-    </div>
+class Select extends Component {
+  constructor(props) {
+    super(props);
 
-    
-  );
+    this.state = {
+      open: false,
+      currVal: '',
+      currLabel: null,
+    };
+
+    this.toggleSelect = this.toggleSelect.bind(this);
+    this.selectOption = this.selectOption.bind(this);
+  }
+
+  toggleSelect(e) {
+    this.setState((prevState) => ({
+      open: !prevState.open,
+    }));
+  }
+
+  selectOption(e) {
+    console.log(e.target);
+    const { onChange } = this.props;
+
+    if (onChange) onChange(currVal, currLabel);
+
+    this.setState({
+      currVal: e.target.getAttribute('data-value'),
+      currLabel: e.target.textContent,
+      open: false,
+    });
+  }
+
+
+  render() {
+    const options = [ { label: '😃 hello', value: 't1' }, { label: '🌊 goodbye', value: 't2' } ];
+    const { open, currVal, currLabel } = this.state;
+    const selectClass = open ? 'active' : '';
+
+    return (
+      <div className={`select ${selectClass}`}>
+        <div className="option-current" onClick={this.toggleSelect} data-value={currVal}>{ currLabel }</div>
+        <div className="options-wrapper">
+          <div className="options">
+            { options.map((option) => <div className={`option${currVal == option.value ? ' selected': ''}`} data-value={option.value} onClick={this.selectOption}>{ option.label }</div>) }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Select;
