9110b6d4bb90025781ccf4b000938794bcd416f8
Vanna <vhluu@ucsd.edu>
2020-02-25 11:54:20 -0800

    rewriting bar graph as a class component in order to automatically scroll if there is overflow

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -1,43 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './BarGraph.scss';
 
-function BarGraph(props) {
-  const { data, max, lineStep, labelStep } = props;
+class BarGraph extends Component {
+  static range(maxVal, step) {
+    return Array.from({ length: (maxVal / step) + 1 }, (x, i) => (maxVal - (i * step)));
+  }
 
-  // Returns an array with values starting from max to 0 (inclusive), with the given step
-  const range = (maxVal, step) => Array.from({ length: (maxVal / step) + 1 }, (x, i) => (maxVal - (i * step)));
+  constructor(props) {
+    super(props);
+    this.barsRef = React.createRef();
+  }  
 
-  const lineValues = range(max, lineStep);
+  componentDidMount() {
+    // get the width of the bars div
+    this.barsWidth = this.barsRef.current.getBoundingClientRect().width;
+    this.resetScroll(); // scroll the bars div to rightmost
+  }
 
-  console.log(lineValues);
+  componentDidUpdate() {
+    this.resetScroll(); // scroll the bars div to rightmost
+  }
 
-  return (
-    <div className="bar-graph">
-      <div className="graph-inner">
-        <div className="x-axis">
-          { lineValues.map((value, index) => {
-            return (
-              <div className="x-axis-lines">
-                { (index % labelStep === 0) && <div className="x-axis-label">{ value }</div> }
-              </div>
-            );
-          }) }
-        </div>
-        <div className="bars">
-          { data.map((point) => {
-            return (
-              <div className="bar-wrapper">
-                <div className="bar" style={{ 'min-height': `calc((${point.value}/${max}) * 100%)` }}>
-                  <div className="bar-value">{ point.value }</div>
+  /* Sets the scroll position to initially display the rightmost item in the graph, if there is overflow */
+  resetScroll() {
+    const barsRef = this.barsRef.current;
+    const { scrollWidth } = barsRef;
+
+    if (scrollWidth > this.barsWidth) { // check if the graph bars are overflowing its container
+      barsRef.scrollLeft = scrollWidth;
+    }
+  }
+
+  render() {
+    const { data, max, lineStep, labelStep } = this.props;
+    const lineValues = BarGraph.range(max, lineStep);
+
+    return (
+      <div className="bar-graph">
+        <div className="graph-inner">
+          <div className="x-axis">
+            { lineValues.map((value, index) => {
+              return (
+                <div className="x-axis-lines">
+                  { (index % labelStep === 0) && <div className="x-axis-label">{ value }</div> }
                 </div>
-                <div className="label">{ point.label }</div>
-              </div>
-            );
-          }) }
+              );
+            }) }
+          </div>
+          <div className="bars" ref={this.barsRef}>
+            { data.map((point) => {
+              return (
+                <div className="bar-wrapper">
+                  <div className="bar" style={{ 'min-height': `calc((${point.value}/${max}) * 100%)` }}>
+                    <div className="bar-value">{ point.value }</div>
+                  </div>
+                  <div className="label">{ point.label }</div>
+                </div>
+              );
+            }) }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default BarGraph;
