0e51b2b4179b7c532994f295ec78c3c015597704
Vanna <vhluu@ucsd.edu>
2020-03-27 17:59:14 -0700

    started cleaning up reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,7 +10,188 @@ const initialState = {
   stats: {},
 };
 
+// Utility function 
+const updateObject = (oldObject, updatedProps) => {
+  return {
+    ...oldObject,
+    ...updatedProps,
+  };
+};
+
+const addTag = (state, action) => {
+  console.log('adding tag', action);
+  return updateObject(state, { 
+    tags: updateObject(state.tags, { [action.tag.id]: action.tag }), 
+    nextId: state.nextId + 1,
+  });
+};
+
+const deleteTag = (state, action) => {
+  console.log('deleting tag', action);
+  // removing tag from tags
+  const { [action.tagId]: deletedItem, ...updatedTags } = state.tags;
+
+  // removing all instances of tag from day tags
+  const updatedDT = {};
+  Object.keys(state.dayTags).forEach((day) => {
+    const filtered = state.dayTags[day].filter((id) => id !== action.tagId);
+    if (filtered.length > 0) updatedDT[day] = filtered;
+  });
+
+  return updateObject(state, {
+    tags: updatedTags,
+    dayTags: updatedDT,
+  });
+};
+
+const updateTag = (state, action) => {
+  console.log('updating tag', action);
+  return updateObject(state, {
+    tags: updateObject(state.tags, { [action.updatedTag.id]: action.updatedTag }),
+  });
+};
+
+const setTags = (state, action) => {
+  console.log('setting tags', action);
+  if (action.tags) {
+    const keys = Object.keys(action.tags);
+    console.log(keys);
+    console.log(keys[keys.length - 1].substring(1));
+    let lastId = keys[keys.length - 1].substring(1);
+    if (Number.isNaN(lastId)) lastId = lastId.substring(1);
+    return {
+      ...state,
+      tags: action.tags,
+      nextId: parseInt(lastId, 10) + 1,
+    };
+  }
+  return {
+    ...state,
+  };
+};
+
+
+const setUser = (state, action) => {
+  console.log('getting user', action);
+  return updateObject(state, { uid: action.userInfo.userId });
+};
+
+const signOutUser = (state, action) => {
+  console.log('signing out user', action);
+  return updateObject(state, {
+    userId: null,
+    tags: {},
+    nextId: 1,
+    dayTags: {},
+    savedStart: null,
+    savedEnd: null,
+  });
+};
+
+const addDayTag = (state, action) => {
+  const currentDT = state.dayTags[action.date];
+
+  return updateObject(state, {
+    dayTags: updateObject(state.dayTags, { 
+      [action.date]: currentDT ? currentDT.concat(action.tagId) : [action.tagId],
+    }),
+  });
+};
+
+const deleteDayTag = (state, action) => {
+  console.log('deleting day tags', action);
+  // removing tags from day tags
+  const filteredTags = state.dayTags[action.date].filter((id) => !action.tags.includes(id));
+  let updatedDT;
+  if (filteredTags.length === 0) { // if all tags are removed from this date, remove the date from dayTags
+    const { [action.date]: value, ...withoutDate } = state.dayTags;
+    updatedDT = withoutDate;
+  } else { // update dayTags with the filtered tagId array for the date
+    updatedDT = {
+      ...state.dayTags,
+      [action.date]: filteredTags,
+    };
+  }
+  return {
+    ...state,
+    dayTags: {
+      ...updatedDT,
+    },
+  };
+};
+
+const setDayTags = (state, action) => {
+  const updatedState = {
+    ...state,
+  };
+
+  if (!state.savedStart || action.start < state.savedStart) {
+    updatedState.savedStart = action.start;
+  }
+  if (!state.savedEnd || action.end > state.savedEnd) {
+    updatedState.savedEnd = action.end;
+  }
+
+  if (action.taggedDays) {
+    const tempObject = { ...action.taggedDays };
+    // for each day, get the tags associated with it and store it as an array
+    Object.keys(action.taggedDays).forEach((day) => {
+      tempObject[day] = Object.keys(action.taggedDays[day]);
+    });
+
+    updatedState.dayTags = {
+      ...state.dayTags,
+      ...tempObject,
+    };
+  } 
+  return updatedState;
+};
+
+const replaceDayTags = (state, action) => {
+  return updateObject(state, {
+    dayTags: updateObject(state.dayTags, { [action.date]: action.tags }),
+  });
+};
+
+const setStats = (state, action) => {
+  const moreStats = updateObject(state.stats, { [action.tagId]: {} });
+  if (action.days) {
+    // add monthly stats for current tag
+    Object.entries(action.days).forEach(([month, days]) => {
+      moreStats[action.tagId][month] = Object.keys(days).length;
+    });
+  }
+
+  return updateObject(state, { stats: moreStats });
+};
+
+const clearStats = (state) => updateObject(state, { stats: {} });
+
+const handlers = {
+  ADD_TAG: addTag,
+  DELETE_TAG: deleteTag,
+  UPDATE_TAG: updateTag,
+  SET_TAGS: setTags,
+  SET_USER: setUser,
+  SIGN_OUT_USER: signOutUser,
+  ADD_DAY_TAG: addDayTag,
+  DELETE_DAY_TAG: deleteDayTag,
+  SET_DAY_TAGS: setDayTags,
+  REPLACE_DAY_TAGS: replaceDayTags,
+  SET_STATS: setStats,
+  CLEAR_STATS: clearStats,
+};
+
 const reducer = (state = initialState, action) => {
+  if (handlers[action.type]) {
+    return handlers[actionTypes[action.type]](state, action);
+  }
+  return state;
+};
+
+/* OLD STUFF STARTS HERE */
+
+/* const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_TAG:
       console.log('adding tag', action);
@@ -174,6 +355,6 @@ const reducer = (state = initialState, action) => {
     default:
       return state;
   }
-};
+}; */
 
 export default reducer;
